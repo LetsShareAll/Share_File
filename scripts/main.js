3cdc2b5c67957e707f6d2bebb8a93171f9befd83
Shuery <2463253700@qq.com>
2023-12-16 16:47:26 +0800

    修复（脚本加载）：未在使用函数前加载函数。

--- scripts/main.js
+++ scripts/main.js
@@ -10,17 +10,27 @@ function loadScript(url, callback) {
   scriptElement.onload = callback;
 
   document.body.appendChild(scriptElement);
-}
+};
 
+// 当 DOMContentLoaded 事件触发时执行以下代码
 document.addEventListener("DOMContentLoaded", function () {
-  // 动态加载脚本
-  loadScript("./file-info.js")
+  // 动态加载脚本文件 file-info.js
+  loadScript("./file-info.js");
+
+  // 动态加载脚本文件 template-utils.js
+  loadScript("/scripts/template-utils.js");
+
+  // 动态加载脚本文件 page-utils.js，并在加载完成后执行回调函数
   loadScript("/scripts/page-utils.js", function () {
-    // page-utils.js 加载完成后执行
+    // 当 page-utils.js 加载完成后执行以下操作
+
     // 设置页面标题
     setPageTitle();
+
+    // 更新文件重定向地址
     updateFileRedirectURL(fileRedirectURL);
   });
-  loadScript("/scripts/template-utils.js");
+
+  // 动态加载脚本文件 load-template.js
   loadScript("/scripts/load-template.js");
 });
