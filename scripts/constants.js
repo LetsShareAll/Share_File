85e41c498014e32186635dd274091f070c75dd3f
Shuery <2463253700@qq.com>
2023-12-17 19:21:14 +0800

    功能（常量定义）：将可配置常量移至单一文件。

---
+++ scripts/constants.js
@@ -0,0 +1,8 @@
+// 要添加到页面标题的后缀，默认为 Share File。
+const pageTitleSuffix = ''
+// 网页头部显示的标题。
+const headerTitle =
+  '<a href="https://github.com/LetsShareAll/Share_File" title="前往 Github 查看源码">一起分享吧！文件！</a>'
+const ERROR_TARGET_NOT_FOUND = `找不到目标元素`
+const ERROR_FETCHING_TEMPLATE = `获取和渲染模板文件时发生错误`
+const ERROR_FETCHING_INSERTING = `获取和插入时发生错误`
