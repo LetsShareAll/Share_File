6d5e222ae691649d8ac2b3960fec94c9b01cce07
Shuery <2463253700@qq.com>
2023-12-18 17:06:04 +0800

    杂项（*）：仅存基础项目文件。

--- file-info.js
+++ file-info.js
@@ -20,45 +20,4 @@ const redirectFileInfo = {
  * @propty {string} date - 文件或文件夹的创建日期。
  * @propty {string} description - 文件或文件夹的描述信息。
  */
-const fileList = [
-  {
-    iconClass: 'icon-directory',
-    name: 'document',
-    type: '文件夹',
-    version: '-',
-    date: '2023/12/15',
-    description: '其他'
-  },
-  {
-    iconClass: 'icon-directory',
-    name: 'other',
-    type: '文件夹',
-    version: '-',
-    date: '2022/05/26',
-    description: '其他'
-  },
-  {
-    iconClass: 'icon-directory',
-    name: 'picture',
-    type: '文件夹',
-    version: '-',
-    date: '2022/06/02',
-    description: '游戏'
-  },
-  {
-    iconClass: 'icon-directory',
-    name: 'software',
-    type: '文件夹',
-    version: '-',
-    date: '2023/12/15',
-    description: '应用程序'
-  },
-  {
-    iconClass: 'icon-directory',
-    name: 'video',
-    type: '文件夹',
-    version: '-',
-    date: '2023/12/14',
-    description: '其他'
-  }
-]
+const fileList = []
